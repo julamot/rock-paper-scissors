4e7571f84d9506e158a7233747fcf22b1a49c079
Julie C. Zachman <jczachman@wisc.edu>
2025-12-11 15:23:18 -0600

    Improve scoring and visuals

--- rps.js
+++ rps.js
@@ -2,69 +2,48 @@ let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice() {
-   let num = Math.random(); 
-   let out = 0;
-   if (num < .33333)
+   const num = Math.random(); 
+   if (num > .66666)
       return "rock";
-   else if (num > .66666)
-      return "scissors";
-   else
+   else if (num > .33333)
       return "paper";
+   else
+      return "scissors";
 
    //console.log(num);
 }
 
+/* Prompt for user input. */
 function getHumanChoice() {
    let human = prompt("What's your sign?");
    return human.toLowerCase();
 }
 
+/* Given human and computer choice, return winner. */
 function playRound(humanChoice, computerChoice) {
+   if (humanChoice == computerChoice)
+   {
+      console.log("Invalid input -- no winner");
+      return -1;
+   }
    let score = 0;
    switch (humanChoice) {
      case "rock":
-       if (computerChoice == "paper")
+       if (computerChoice == "scissors")
        {
-          console.log("You lose! Paper beats Rock");
-       }
-       else if (computerChoice == "scissors")
-       {
-          console.log("You win! Rock beats Scissors");
           score = 1;
-       }
-       else
-       {
-          console.log("No winner");
        }
        break;
      case "paper":
        if (computerChoice == "rock")
        {
-          console.log("You win! Paper beats Rock");
-       }
-       else if (computerChoice == "scissors")
-       {
-          console.log("You lose! Scissors beats Paper");
-       }
-       else
-       {
-          console.log("No winner");
+          score = 1;
        }
        break;
      case "scissors":
-       //console.log ("Human chose scissors");
-       if (computerChoice == "rock")
+       if (computerChoice == "paper")
        {
-          console.log("You lose! Rock beats Scissors");
-       }
-       else if (computerChoice == "paper")
-       {
-          console.log("You win! Scissors beats Paper");
 			    score = 1;
-       }
-       else
-       {
-          console.log("No winner");
        }
        break;
      default:
@@ -73,6 +52,7 @@ function playRound(humanChoice, computerChoice) {
    return score;
 }
 
+/* Helper function, get the choices, play, return score. */
 function playOne() {
 
    const humanSelection  = getHumanChoice();
@@ -80,22 +60,37 @@ function playOne() {
    
    while (humanSelection == computerSelection)
    {
-      console.log("Try again, poo-ter");
       computerSelection=getComputerChoice(); 
    }
-   console.log("Computer: " + computerSelection);
-   const score = playRound(humanSelection, computerSelection);
+   console.log("Human, Computer: " + humanSelection + ", " + computerSelection);
+   return playRound(humanSelection, computerSelection);
 }
 
-//playOne();
+//console.log("Your score is " + playOne());
+
 function playGame() {
 
    n = 5;
-   let humanScore=0;
+   let roundScore=0;   
+   let humanScore = 0;
+   let computerScore = n;
+   let i=0;
 
-   for (i=0; i<n; i++) 
+   for (i=1; i<=n; i++) 
    {
-      humanScore += playOne(); 
+      roundScore = playOne();
+      //console.log("roundScore is " + roundScore);
+      humanScore += roundScore; 
+      computerScore = i - humanScore;
+      if (roundScore)
+      {
+         console.log (i + ". HUMAN: " + humanScore + " -- Computer: " + computerScore);
+      }
+      else
+      {
+         console.log (i + ". Human: " + humanScore + " -- COMPUTER: " + computerScore);
+      }
    }
-   const computerScore = n-humanScore; 
 }
+
+playGame();
